69f319b2c146de115842c9bdacf23ef61116f29c
toom <tiraphan@gmail.com>
2024-12-04 18:03:33 +0700

    Update App

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,34 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  List tabs = ["All", "Category", "Top", "Recommended"];
+
+  List imageList = [
+    "images/image1.jpg",
+    "images/image2.jpg",
+    "images/image3.jpg",
+    "images/image4.jpg",
+  ];
+
+  List ProductTitles = [
+    "Warm Zipper",
+    "Knitted Woo!",
+    "Zipper Win",
+    "Child Win",
+  ];
+
+  List prices = [
+    "\$300",
+    "\$650",
+    "\$50",
+    "\$100",
+  ];
+  List reviews = [
+    "54",
+    "120",
+    "542",
+    "34",
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -25,27 +52,145 @@ class HomeScreen extends StatelessWidget {
                         borderRadius: BorderRadius.circular(10),
                         boxShadow: [
                           BoxShadow(
-                            color: Colors.black12,
-                            blurRadius: 1,
+                            color: Colors.black12.withOpacity(0.05),
+                            blurRadius: 2,
                             spreadRadius: 1,
                           ),
                         ],
                       ),
                       child: TextFormField(
                         decoration: InputDecoration(
-                            prefixIcon: Icon(
-                              Icons.search,
-                              color: Color(0xFFDB3022),
-                            ),
-                            border: InputBorder.none,
-                            label: Text(
-                              "Find your product",
-                              style: TextStyle(),
-                            )),
+                          prefixIcon: Icon(
+                            Icons.search,
+                            color: Color(0xFFDB3022),
+                          ),
+                          border: InputBorder.none,
+                          label: Text(
+                            "Find your product",
+                            style: TextStyle(),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      height: 50,
+                      width: MediaQuery.of(context).size.width / 6,
+                      decoration: BoxDecoration(
+                        color: Colors.black12.withOpacity(0.05),
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black12.withOpacity(0.05),
+                            blurRadius: 2,
+                            spreadRadius: 1,
+                          )
+                        ],
+                      ),
+                      child: Center(
+                        child: Icon(
+                          Icons.notifications,
+                          color: Color(0xFFDB3022),
+                        ),
                       ),
                     ),
                   ],
                 ),
+                SizedBox(height: 20),
+                Container(
+                  height: 150,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                    color: Color(0xFFFFF0DD),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Image.asset("images/freed.png"),
+                ),
+                SizedBox(height: 20),
+                SizedBox(
+                  height: 50,
+                  child: ListView.builder(
+                    shrinkWrap: true,
+                    scrollDirection: Axis.horizontal,
+                    itemCount: tabs.length,
+                    itemBuilder: (context, index) {
+                      return FittedBox(
+                        child: Container(
+                          height: 40,
+                          margin: EdgeInsets.all(8),
+                          padding: EdgeInsets.only(left: 15, right: 15),
+                          decoration: BoxDecoration(
+                            color: Colors.black12.withOpacity(0.05),
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: Center(
+                            child: FittedBox(
+                              child: Text(
+                                tabs[index],
+                                style: TextStyle(
+                                  color: Colors.black38,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+                SizedBox(height: 20),
+                Container(
+                  color: Colors.red,
+                  height: 200,
+                  child: ListView.builder(
+                    itemCount: imageList.length,
+                    scrollDirection: Axis.horizontal,
+                    shrinkWrap: true,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            SizedBox(
+                              height: 200,
+                              child: Stack(
+                                children: [
+                                  InkWell(
+                                    onTap: () {},
+                                    child: ClipRRect(
+                                      borderRadius: BorderRadius.circular(20),
+                                      child: Image.asset(imageList[index]),
+                                    ),
+                                  ),
+                                  Positioned(
+                                    right: 10,
+                                    top: 10,
+                                    child: Container(
+                                      height: 30,
+                                      width: 30,
+                                      decoration: BoxDecoration(
+                                        color: Colors.white,
+                                        borderRadius: BorderRadius.circular(20),
+                                      ),
+                                      child: Center(
+                                        child: Icon(
+                                          Icons.favorite,
+                                          color: Color(0xFFDB3022),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                )
               ],
             ),
           ),
